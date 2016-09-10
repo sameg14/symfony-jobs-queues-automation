bf94c38a816d5affe82f7a2597ced056745b1b5a
Samir Patel <sameg14@gmail.com>
2016-09-10 18:51:00 -0500

    Added comments

--- src/AppBundle/Controller/WorkerController.php
+++ src/AppBundle/Controller/WorkerController.php
@@ -11,6 +11,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 /**
  * Class WorkerController is responsible for scheduling and executing workers
+ * This controller also supports endpoints to get the status of a job and to return any data that this job may have produced
  * @package AppBundle\Controller
  */
 class WorkerController extends Controller
@@ -33,15 +34,22 @@ class WorkerController extends Controller
      */
     public function scheduleEmailWorkerAction(Request $request)
     {
+        // Get the email address entered by the user
         $email = $request->get('email');
 
+        // Get the job runner service from the service container AppBundle\Resources\config\services.yml
         $runner = $this->get('service.job_runner');
 
+        // Tell the runner which worker will handle this job
         $runner->setWorkerClass(HackerNewsEmailWorker::class);
+
+        // What data do we need to associate with the job to give to the worker
         $runner->setJobData(['email' => $email]);
 
+        // Scheduling a job for deferred execution will return a jobId
         $jobId = $runner->schedule();
 
+        // Render a template with the jobId and some other actions
         return $this->render('AppBundle:Demo:demo.scheduled.html.twig', [
             'jobId' => $jobId
         ]);
